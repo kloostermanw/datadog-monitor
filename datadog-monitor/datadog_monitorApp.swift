67dd95f1affefafb832afc4ebbb7140908d1f44b
Wiebe <wiebe@celerypayroll.com>
2023-12-13 19:38:35 -0400

    Initial Commit

---
+++ datadog-monitor/datadog_monitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  datadog_monitorApp.swift
+//  datadog-monitor
+//
+//  Created by Wiebe on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct datadog_monitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
